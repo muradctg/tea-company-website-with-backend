c4215ecc6177480446672d953136d6fe0e190093
muradctg <wri_murad@yahoo.com>
2022-06-14 17:17:45 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+//setup date
+const date = (document.getElementById("date").innerHTML =
+  new Date().getFullYear());
+
+// setup nav
+const navBtn = document.getElementById("nav-btn");
+const navbar = document.getElementById("navbar");
+const navClose = document.getElementById("nav-close");
+// show nav
+navBtn.addEventListener("click", () => {
+  navbar.classList.add("showNav");
+});
+// close nav
+navClose.addEventListener("click", () => {
+  navbar.classList.remove("showNav");
+});
+
+const formDOM = document.querySelector(".contact-form-js");
+const nameInputDOM = document.querySelector(".contact-name");
+const emailInputDOM = document.querySelector(".contact-email");
+const msgInputDOM = document.querySelector(".contact-msg");
+
+formDOM.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const name = nameInputDOM.value;
+  const email = emailInputDOM.value;
+  const msg = msgInputDOM.value;
+
+  try {
+    await axios.post("http://localhost:5000/api/v1/contact-form", {
+      name,
+      email,
+      msg,
+    });
+    nameInputDOM.value = "";
+    emailInputDOM.value = "";
+    msgInputDOM.value = "";
+  } catch (error) {
+    console.log(name, email, msg);
+    console.log("error");
+  }
+});
